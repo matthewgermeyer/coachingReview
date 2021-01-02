957825f50728a70b077116d06dcbc64a2ec79fb9
Matthew Germeyer <matthew.germeyer@gmail.com>
2021-01-02 00:12:28 -0600

    inital commit

---
+++ main.js
@@ -0,0 +1,22 @@
+//Return a random letter from your name.
+
+const myName = 'matt';
+console.log(myName);
+
+const Hugo = {
+    'name' : 'Hugo',
+    'position' : 'GK',
+}
+
+// Player class
+// class Player(name, position) {
+//    "name";
+//    "position";
+     
+// }
+
+
+
+//Roster
+const roster = [Hugo]
+console.log(roster);
